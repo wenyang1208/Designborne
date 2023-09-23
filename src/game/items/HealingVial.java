873e220575015b7f1d3cf336d03fc17e2f912597
RuiEnKoe <rkoe0003@student.monash.edu>
2023-09-23 23:20:37 +0800

    Amend RefreshingFlask and HealingVial classes

--- src/game/items/HealingVial.java
+++ src/game/items/HealingVial.java
@@ -1,3 +1,4 @@
+//
 package game.items;
 
 import edu.monash.fit2099.engine.actions.ActionList;
@@ -8,36 +9,63 @@ import edu.monash.fit2099.engine.items.Item;
 import game.actions.ConsumeAction;
 
 /**
- * class representing healing vial
+ * A class that represents the HealingVial item for the player to increase their health.
+ *
+ * Created by:
+ * @author Koe Rui En
+ *
+ * Modified by:
+ * @author Yang Yang Dan
+ *
  */
 public class HealingVial extends Item implements Consumable{
 
+    // healing percentage
+    /**
+     * A healing percentage to heal the holder/owner of HealingVial item
+     */
+    private final float healingPercentage;
 
     /**
-     * Constructor
+     * Constructor of HealingVial class
+     *
      * It has the ability to increase health point after consumption
      */
     public HealingVial() {
+
         super("Healing vial", 'a', true);
+        this.healingPercentage = 0.1f;
+
     }
 
-
     /**
-     * A healing item can have a special skill that can increase the current actor's hit points.
+     * A HealingVial item can have a special skill that can increase the current actor's hit points.
+     *
      * @param owner the actor that owns the item
-     * @return an unmodifiable list of Actions
+     *
+     * @return a list of Actions for actor acts on the HealingVial item
      */
     @Override
     public ActionList allowableActions(Actor owner) {
+
         return new ActionList( new ConsumeAction(this) );
+
     }
 
-
+    /**
+     * A consume method to allow owner consume the item
+     *
+     * @param owner the owner of a consumed item
+     */
     @Override
-    public String consume(Actor actor) {
-        int value = (int)(0.1 * actor.getAttributeMaximum(BaseActorAttributes.HEALTH));
-        actor.modifyAttribute(BaseActorAttributes.HEALTH, ActorAttributeOperations.INCREASE, value);
-        actor.removeItemFromInventory(this);
-        return actor + " consumes " + this + ", and it restores the health by " + value + " points.";
+    public String consume(Actor owner) {
+
+        int value = (int)(healingPercentage * owner.getAttributeMaximum(BaseActorAttributes.HEALTH));
+
+        owner.modifyAttribute(BaseActorAttributes.HEALTH, ActorAttributeOperations.INCREASE, value);
+        owner.removeItemFromInventory(this);
+
+        return owner + " consumes " + this + ", and it restores the health by " + value + " points.";
+
     }
 }
--- src/game/items/RefreshingFlask.java
+++ src/game/items/RefreshingFlask.java
@@ -1,5 +1,7 @@
+// declare package
 package game.items;
 
+// import game and engine packages
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
@@ -8,36 +10,65 @@ import edu.monash.fit2099.engine.items.Item;
 import game.actions.ConsumeAction;
 
 /**
- * class representing refreshing flask
+ * A class that represents the RefreshingFlask for the player to increase their stamina
+ *
+ * Created by:
+ * @author Koe Rui En
+ *
+ * Modified by:
+ * @author Yang Yang Dan
+ *
  */
 public class RefreshingFlask extends Item implements Consumable{
 
+    // healing percentage
+    /**
+     * A healing percentage to heal the holder/owner of RefreshingFlask item
+     */
+    private final float healingPercentage;
 
     /**
-     * Constructor
+     * Constructor for the RefreshingFlask class
+     *
      * It has the ability to increase stamina point after consumption
      */
     public RefreshingFlask() {
+
         super("Refreshing flask", 'u', true);
+
+        // increase their stamina by 20% of their maximum stamina
+        this.healingPercentage = 0.2f;
     }
 
 
     /**
-     * A refreshing flask item can have a special skill that can increase the current actor's stamina points.
+     * A refreshing flask item can have a special skill that can increase the current actor's stamina points
+     *
      * @param owner the actor that owns the item
-     * @return an unmodifiable list of Actions
+     *
+     * @return a list of Actions for actor acts on the RefreshingFlask item
      */
     @Override
     public ActionList allowableActions(Actor owner) {
+
         return new ActionList( new ConsumeAction(this) );
+
     }
 
-
+    /**
+     * A consume method to allow owner consume the item
+     *
+     * @param owner the owner of a consumed item
+     */
     @Override
-    public String consume(Actor actor) {
-        int value = (int)(0.2 * actor.getAttributeMaximum(BaseActorAttributes.STAMINA));
-        actor.modifyAttribute(BaseActorAttributes.STAMINA, ActorAttributeOperations.INCREASE, value);
-        actor.removeItemFromInventory(this);
-        return actor + " consumes " + this + ", and it restores the stamina by " + value + " points.";
+    public String consume(Actor owner) {
+
+        int value = (int)(healingPercentage * owner.getAttributeMaximum(BaseActorAttributes.STAMINA));
+
+        owner.modifyAttribute(BaseActorAttributes.STAMINA, ActorAttributeOperations.INCREASE, value);
+        owner.removeItemFromInventory(this);
+
+        return owner + " consumes " + this + ", and it restores the stamina by " + value + " points.";
+
     }
 }
