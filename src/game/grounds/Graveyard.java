e46ea163ef7f469310441cf85f42e0097c2845d2
RuiEnKoe <rkoe0003@student.monash.edu>
2023-09-23 22:51:33 +0800

    Create new abstract class, Spawner and amend Graveyard class

--- src/game/grounds/Graveyard.java
+++ src/game/grounds/Graveyard.java
@@ -4,7 +4,7 @@ package game.grounds;
 // import engine and game packages
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
-import game.actors.Enemy;
+import game.actors.npc.Enemy;
 
 /**
  * A class that represents the graveyard in the different game maps
@@ -12,24 +12,16 @@ import game.actors.Enemy;
  * Created by:
  * @author Koe Rui En
  *
+ * Modified by:
+ * @author Koe Rui En
+ *
  */
 // there are several graveyards in the village
-public class Graveyard extends Ground{
-
-    // declare instance variables
-
-    // save attributes of enemies
-    /**
-     * location of gate to travel to
-     */
-    private Enemy enemy;
-
-    // spawn percentage
-    private double spawnPercentage;
+public class Graveyard extends Spawner{
 
 
     /**
-     * Parameterised constructor of the Graveyard class
+     * Constructor of the Graveyard class
      *
      * @param actor actor to be spawned in certain chance of time
      */
@@ -37,44 +29,9 @@ public class Graveyard extends Ground{
 
         // initialise instance of graveyard
         // The graveyard is displayed as “n”.
-        super('n');
-
-        enemy = actor;
-
-        spawnPercentage = iniSpawnPercentage;
-
+        super('n', actor, iniSpawnPercentage);
 
     }
-
-
-    /**
-     * Graveyard can also experience the joy of time.
-     * A chance to spawn either Hollow Soldier or Wandering Undead at every turn of game
-     *
-     * @param location The location of the Graveyard
-     */
-    @Override
-    public void tick(Location location) {
-
-
-        // check location contains wandering undead and chance to spawn with 25%  at every turn of game
-        if (Math.random() <= spawnPercentage && !location.containsAnActor()) {
-
-            // spawn the wandering undead
-            // add spawned enemies to the location
-            location.addActor(enemy.spawnMethod());
-
-        }
-
-    }
-
-
-
-
-
-
-
-
 
 
 }
---
+++ src/game/grounds/Spawner.java
@@ -0,0 +1,71 @@
+// declare package
+package game.grounds;
+
+// import engine and game packages
+
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.npc.Enemy;
+
+/**
+ * An abstract class to spawn different enemies in the different game maps
+ *
+ * Created by:
+ * @author Koe Rui En
+ *
+ */
+public abstract class Spawner extends Ground {
+
+    // declare instance variables
+
+    // save attributes of enemies
+    /**
+     * enemy to be spawned
+     */
+    private Enemy enemy;
+
+    // spawn percentage
+    /**
+     * percentage to spawn an enemy
+     */
+    private double spawnPercentage;
+
+
+    /**
+     * Parameterised constructor of the abstract Spawner class
+     *
+     * @param displayChar character to display for this type of terrain
+     * @param actor actor to be spawned in certain chance of time
+     */
+    public Spawner(char displayChar, Enemy actor, double iniSpawnPercentage) {
+
+        super(displayChar);
+
+        enemy = actor;
+
+        spawnPercentage = iniSpawnPercentage;
+
+    }
+
+
+    /**
+     * Spawner abstract class can also experience the joy of time.
+     * A chance to spawn different enemies at every turn of game
+     *
+     * @param location The location of the ground that extends Spawner abstract class
+     */
+    @Override
+    public void tick(Location location) {
+
+
+        // check location contains enemies and chance to spawn at certain percentage at every turn of game
+        if (Math.random() <= spawnPercentage && !location.containsAnActor()) {
+
+            // add spawned enemies to the location
+            location.addActor(enemy.spawnMethod());
+
+        }
+
+    }
+
+}
