16645cb42b9bf3b1a9a3c41d68e599c0f4f4e844
RuiEnKoe <rkoe0003@student.monash.edu>
2023-09-24 00:42:07 +0800

    Amend Graveyard constructor's attribute name

--- src/game/grounds/Graveyard.java
+++ src/game/grounds/Graveyard.java
@@ -2,34 +2,29 @@
 package game.grounds;
 
 // import engine and game packages
-import edu.monash.fit2099.engine.positions.Ground;
-import edu.monash.fit2099.engine.positions.Location;
 import game.actors.npc.Enemy;
 
 /**
- * A class that represents the graveyard in the different game maps
+ * A class that represents the graveyard in the different game maps (Abandoned Village and Burial Ground Maps)
  *
  * Created by:
- * @author Koe Rui En
- *
- * Modified by:
  * @author Koe Rui En
  *
  */
 // there are several graveyards in the village
 public class Graveyard extends Spawner{
 
-
     /**
      * Constructor of the Graveyard class
      *
-     * @param actor actor to be spawned in certain chance of time
+     * @param enemy enemy to be spawned in certain chance of time
+     * @param iniSpawnPercentage percentage to spawn an enemy
      */
-    public Graveyard(Enemy actor, double iniSpawnPercentage) {
+    public Graveyard(Enemy enemy, double iniSpawnPercentage) {
 
         // initialise instance of graveyard
         // The graveyard is displayed as “n”.
-        super('n', actor, iniSpawnPercentage);
+        super('n', enemy, iniSpawnPercentage);
 
     }
 
